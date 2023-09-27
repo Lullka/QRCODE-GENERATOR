35f88894ad07cab7e9d7e06edf6fb72df9e236d6
Luka <87625839+Lullka@users.noreply.github.com>
2023-09-26 23:20:20 -0300

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ ContainerQRCODE.classList.remove("showed");
 
 function generateQR(){
     if(QrInput.value.trim() == "") return;
-    QRCODEImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=$" + QrInput.value;
+    QRCODEImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + QrInput.value;
     QRCODEImg.addEventListener('load', () => {
         ContainerQRCODE.classList.add("showed");
         InformationMoment.innerText = "QRCode gerado!";
